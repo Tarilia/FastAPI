e09292ad0aadc1cd9ecad4a62aa1e6a910e0c223
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-03-08 15:06:06 +0300

    add TaskCreate

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from typing import Annotated
 
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': 'Hello'}
+class TaskCreate(BaseModel):
+    name: str
+    description: str | None = None
+
+
+class TaskRead(TaskCreate):
+    id: int
+
+
+tasks = []
+
+
+@app.post('/tasks')
+async def task_create(
+        task: Annotated[TaskCreate, Depends()]
+):
+    tasks.append(task)
+    return {'ok': True}
